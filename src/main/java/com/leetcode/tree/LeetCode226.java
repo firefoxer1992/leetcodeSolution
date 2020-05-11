21e7fc94a3a2c8a1d0c7a3a7238b74caeeb8ff5e
fenglin <475534279@qq.com>
2020-05-11 23:48:40 +0800

    利用递归，翻转二叉树

---
+++ src/main/java/com/leetcode/tree/LeetCode226.java
@@ -0,0 +1,35 @@
+package com.leetcode.tree;
+
+/**
+ * @Author: EnjoyCoding
+ * @Date: 2020\5\11 0011 22:43
+ * @Description:
+
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+	public TreeNode invertTree(TreeNode root) {
+		if(root==null) {
+			return null;
+		}
+		TreeNode temp=root.left;
+		root.left=root.right;
+		root.right=temp;
+		invertTree(root.left);
+		invertTree(root.right);
+		return root;
+
+	}
+}
